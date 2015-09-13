90de2ff7d8310e1fea9469b43f4832400d5e6025
jimbo644664 <james.arcus64@gmail.com>
2015-09-14 00:11:26 +0800

    Create tracker2.py

---
+++ tracker2.py
@@ -0,0 +1,56 @@
+import os
+import io
+from collections import Counter
+
+class Tracker:
+    def __init__(self, table='tracker', data_dir='./tracker', entry_size=4):
+        self.ENTRY_SIZE = entry_size
+        self.ROW_SIZE = self.ENTRY_SIZE * 2
+        self.DATA_DIR = data_dir
+        self.DAT_PATH = self.DATA_DIR + '/' + table + '.dat'
+        
+        os.makedirs(data_dir, exist_ok=True)
+        if not os.path.exists(self.DAT_PATH):
+            open(self.DAT_PATH, mode='ab').close()
+
+        self.counter = Counter()
+
+    def get_stat(self, story_id):
+        return self.counter[story_id]
+
+    def set_stat(self, story_id, val):
+        self.counter[story_id] = val
+
+    def mod_stat(self, story_id, off):
+        self.counter[story_id] += off
+        if self.counter[story_id] < 0:
+            self.counter[story_id] = 0
+
+    def inc_stat(self, story_id):
+        self.counter[story_id] += 1
+
+    def write(self):
+        with open(self.DAT_PATH, mode='wb') as file:
+            for i in self.counter:
+                file.write(i.to_bytes(self.ENTRY_SIZE, byteorder='little'))
+                file.write(self.counter[i].to_bytes(self.ENTRY_SIZE, byteorder='little'))
+
+    def read(self):
+        with open(self.DAT_PATH, mode='rb') as file:
+            while True:
+                chunk = file.read(self.ROW_SIZE)
+                if not chunk:
+                    break
+                
+                sid_b = chunk[0:self.ENTRY_SIZE]
+                val_b = chunk[self.ENTRY_SIZE:self.ROW_SIZE]
+                    
+                sid = int.from_bytes(sid_b, byteorder='little')
+                val = int.from_bytes(val_b, byteorder='little')
+
+                self.counter[sid] = val
+
+    def extract(self, out_file='dump.csv'):
+        with open(self.DATA_DIR+'/'+out_file, mode='w') as file:
+            for i in self.counter:
+                file.write(str(i) + ',' + str(self.counter[i]) + '\n')
