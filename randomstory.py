4e7df25d6611814057d70763dd87fd2122f6b0fa
jimbo644664 <james.arcus64@gmail.com>
2015-09-16 18:49:22 +0800

    Update randomstory.py

--- randomstory.py
+++ randomstory.py
@@ -1,25 +1,68 @@
+from collections import OrderedDict
+
 import ffparser
 import random
 import requests
 import webbrowser
-from collections import OrderedDict
+import inputmod
 
 domain = 'https://www.fanfiction.net'
 fandoms = {
     'Harry Potter': '/book/Harry-Potter/'
 }
 
-restrict = {'srt': 1, 'lan': 1, 'r': 10, 'len': 100} # sorted by updates, English,, all ratings, length > 100k 
+restrict = {'lan': 1}
+
+ratings = OrderedDict([
+    ('All', 10),
+    ('K->T', 103),
+    ('K->K+', 102),
+    ('K', 1),
+    ('K+', 2),
+    ('T', 3),
+    ('M', 4)
+])
+
+length = OrderedDict([
+    ('<1k', 11),
+    ('<5k', 51),
+    ('>1k', 1),
+    ('>5k', 5),
+    ('>10k', 10),
+    ('>20k', 20),
+    ('>40k', 40),
+    ('>60k', 60),
+    ('>100k', 100)
+])
+
+sorts = OrderedDict([
+    ('Updated', 1),
+    ('Published', 2),
+    ('Reviews', 3),
+    ('Favourites', 4),
+    ('Follows', 5),
+])
+
+uc = inputmod.UserChoice('Rating of fics?', ratings)
+restrict['r'] = uc.prompt()
+
+uc = inputmod.UserChoice('Length of fics?', length)
+restrict['len'] = uc.prompt()
+
+uc = inputmod.UserChoice('Sorted by?', sorts)
+restrict['srt'] = uc.prompt()
+
 fpage = requests.get(domain+fandoms['Harry Potter'], params=restrict)
 
-# we get all the links to more results on the page, in order to find the total number of pages
 l = ffparser.LinkExtractor(fandoms['Harry Potter'], params=restrict)
 l.feed(fpage.text)
 
-# split each link into it's get parameters
-pages = [int(x.split('&')[-1][2:]) for x in l.output_list] # page number is always the last get parameter
+pages = [int(x.split('&')[-1][2:]) for x in l.output_list]
 pages.sort()
 lpage = pages[-1]
+
+uc = inputmod.UserChoice('First n pages?', range(1, lpage+1))
+lpage = uc.prompt()
 
 random.seed()
 npage = random.randint(1, lpage)
@@ -27,7 +70,6 @@ npage = random.randint(1, lpage)
 restrict['p'] = npage
 rpage = requests.get(domain+fandoms['Harry Potter'], params=restrict)
 
-# get all the story links on the current random page
 l = ffparser.LinkExtractor('/s/')
 l.feed(rpage.text)
 
