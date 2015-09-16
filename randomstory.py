117cd126f3121ad68ade839ae9a4294483a89ec3
jimbo644664 <james.arcus64@gmail.com>
2015-09-16 15:40:09 +0800

    Update randomstory.py

--- randomstory.py
+++ randomstory.py
@@ -9,13 +9,15 @@ fandoms = {
     'Harry Potter': '/book/Harry-Potter/'
 }
 
-restrict = {'srt': 1, 'lan': 1, 'r': 10, 'len': 100}
+restrict = {'srt': 1, 'lan': 1, 'r': 10, 'len': 100} # sorted by updates, English,, all ratings, length > 100k 
 fpage = requests.get(domain+fandoms['Harry Potter'], params=restrict)
 
+# we get all the links to more results on the page, in order to find the total number of pages
 l = ffparser.LinkExtractor(fandoms['Harry Potter'], params=restrict)
 l.feed(fpage.text)
 
-pages = [int(x.split('&')[-1][2:]) for x in l.output_list]
+# split each link into it's get parameters
+pages = [int(x.split('&')[-1][2:]) for x in l.output_list] # page number is always the last get parameter
 pages.sort()
 lpage = pages[-1]
 
@@ -25,6 +27,7 @@ npage = random.randint(1, lpage)
 restrict['p'] = npage
 rpage = requests.get(domain+fandoms['Harry Potter'], params=restrict)
 
+# get all the story links on the current random page
 l = ffparser.LinkExtractor('/s/')
 l.feed(rpage.text)
 
